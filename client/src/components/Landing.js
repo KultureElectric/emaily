2b38f72b08656a60c3e542893503d7577fc61840
ruben <rubenusshd@gmail.com>
2018-11-02 11:39:03 +0100

    added simple landing page and made the logo clickable

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1 style={{ textAlign: "center" }}>Emaily!</h1>
+      Collect feedback from your users
+    </div>
+  );
+};
+
+export default Landing;
