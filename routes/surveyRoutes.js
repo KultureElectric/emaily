50632508920843818be39ab97ea39ec5f91116b4
ruben <rubenusshd@gmail.com>
2018-11-06 17:50:33 +0100

    created mailer object and set up to send out emails

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,11 +1,13 @@
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 const mongoose = require("mongoose");
+const Mailer = require("../services/Mailer");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
@@ -16,5 +18,9 @@ module.exports = app => {
       _user: req.user.id,
       dateSent: Date.now()
     });
+
+    // Great place to send an email!
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    await mailer.send();
   });
 };
