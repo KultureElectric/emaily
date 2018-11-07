d0d406f7576a9168b5417655832a55d782fc330e
ruben <rubenusshd@gmail.com>
2018-11-07 13:44:46 +0100

    added email answer redirect route and saved user after mail was send

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,6 +7,10 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
+  app.get("/api/surveys/thanks", (req, res) => {
+    res.send("Thanks for voting!");
+  });
+
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
@@ -21,6 +25,16 @@ module.exports = app => {
 
     // Great place to send an email!
     const mailer = new Mailer(survey, surveyTemplate(survey));
-    await mailer.send();
+
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (e) {
+      res.status(422).send(err);
+    }
   });
 };
