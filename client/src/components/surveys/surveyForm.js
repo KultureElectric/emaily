80e7d308977757484897773c93a033ba50339f5d
ruben <rubenusshd@gmail.com>
2018-11-10 14:53:55 +0100

    create simple surfeyfield component and added logic to survey new to render it 4 times

--- client/src/components/surveys/surveyForm.js
+++ client/src/components/surveys/surveyForm.js
@@ -1,11 +1,45 @@
 import React from "react";
+import _ from "lodash";
 import { reduxForm, Field } from "redux-form";
+import { Link } from "react-router-dom";
+
+import SurveyField from "./surveyField";
+
+const FIELDS = [
+  { label: "Survey Title", name: "title" },
+  { label: "Subject Line", name: "sibject" },
+  { label: "Email Body", name: "body" },
+  { label: "Recipient List", name: "emails" }
+];
 
 class SurveyForm extends React.Component {
+  renderFields() {
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
+        <Field
+          key={name}
+          component={SurveyField}
+          type="text"
+          label={label}
+          name={name}
+        />
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-        <Field type="text" name="surveyTitle" component="input" />
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {this.renderFields()}
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
+        </form>
       </div>
     );
   }
