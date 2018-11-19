869a067bab94d96d794cc84cc0ff1dd9e12c4493
ruben <rubenusshd@gmail.com>
2018-11-19 20:33:38 +0100

    grabbed emailFrom value and intergrated it within the survey model

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -36,7 +36,7 @@ module.exports = app => {
   });
 
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
-    const { title, subject, body, recipients } = req.body;
+    const { title, subject, body, recipients, emailFrom } = req.body;
 
     const survey = new Survey({
       title,
@@ -44,7 +44,8 @@ module.exports = app => {
       body,
       recipients: recipients.split(",").map(email => ({ email: email.trim() })),
       _user: req.user.id,
-      dateSent: Date.now()
+      dateSent: Date.now(),
+      emailFrom
     });
 
     // Great place to send an email!
